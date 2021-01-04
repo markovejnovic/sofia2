ed307ad386d56ddaccec204822970311ebd527fc
Marko Vejnovic <marko.vejnovic@hotmail.com>
2021-01-04 21:05:10 +0100

    Wrote test boilerplate

--- tests/apiview.py
+++ tests/apiview.py
@@ -17,6 +17,49 @@
 """Contains the tests for the API view."""
 
 import unittest
+import json
+import requests
 
-class TestAPIView(unittest.TestCase):
+class TestDevicesRoute(unittest.TestCase):
     """Tests whether the API view adheres to the API requirements."""
+
+    def setUp():
+        # TODO: Start the REST server
+        pass
+
+    def test_empty(self):
+        """Tests whether the API correctly returns the information for 0
+        devices."""
+        mdata = json.loads(requests.get('http://localhost:8080/devices'))
+
+        self.assertEqual(0, len(mdata))
+
+    def test_one_device(self):
+        """Tests whether the API correctly returns the information for one
+        device."""
+        class LedDevice(Device):
+            def on_register(self):
+                self.test_is_on = False
+
+            def on_deregister(self):
+                self.test_is_on = False
+
+            def _set_state(self, state):
+                self.test_is_on = state
+
+            def get_handlers(self):
+                return {
+                    'TEMP_HIGH': [lambda msg: self._set_state(True)],
+                    'TSENSE_ON': [lambda msg: self._set_state(False)]
+                }
+        self.led_device = LedDevice()
+        self.device_manager.register_device(self.led_device)
+
+        mdata = json.loads(requests.get('http://localhost:8080/devices'))
+        self.assertEqual(1, len(mdata))
+        self.assertEqual([
+            {
+                'name': 'LedDevice',
+                'handlers': [ 'TEMP_SENSE', 'TSENSE_ON' ]
+            }
+        ], mdata)
